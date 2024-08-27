a7ffd5aff66615b38a5e4829af49cadf0bc5f7ed
unknown <ustolaunknown@gmail.com>
2024-08-27 20:18:52 +0530

    setting up nav again

--- src/components/Navbars.jsx
+++ src/components/Navbars.jsx
@@ -14,21 +14,25 @@ const Navbars = () => {
     <nav className="">
         <div className="flex items-center font-medium justify-around">
             <div className="z-50 p-4 md:w-auto w-full flex justify-between ">
-                <span className="text-4xl font-bold ">NS Solution</span>
+                <span className="text-4xl font-bold ">
+                    <Link to='/' >
+                         NS Solution
+                    </Link>
+                    </span>
                 <div className="text-3xl md:hidden" onClick={()=>setOpen(!open)}>
                 <ion-icon name={`${open ? "close" : "menu"}`}></ion-icon>
                 </div>
             </div>
             <ul className="md:flex hidden uppercase items-center gap-8 font-[poppins] ">
-                <li>
+                {/* <li>
                     <Link to="/" className="py-7 px-3 inline-block">Home</Link>
-                </li>
+                </li> */}
                 <NavLinks/>
                 <li>
-                    <Link to="/Contact" className="py-7 px-3 inline-block ">Contact</Link>
+                    <Link to="/contact" className="py-7 px-3 inline-block ">Contact</Link>
                 </li>
                 <li>
-                    <Link to="/About" className="py-7 px-3 inline-block">About</Link>
+                    <Link to="/about" className="py-7 px-3 inline-block">About</Link>
                 </li>
             </ul>
             {/*mobile nav*/ }
@@ -36,15 +40,15 @@ const Navbars = () => {
                     md:hidden absolute bg-orange-400 z-10  w-full h-85 top-0 py-24 pl-4
                     duration-500 ${open ? "left-0" : "left-[-100%]"}
                 `}>
-                <li>
+                {/* <li>
                     <Link to="/" className="py-7 px-3 inline-block border-b">Home</Link>
-                </li>
+                </li> */}
                 <NavLinks/>
                 <li>
-                    <Link to="/Contact" className="py-7 px-3 inline-block border-b ">Contact</Link>
+                    <Link to="/contact" className="py-7 px-3 inline-block border-b ">Contact</Link>
                 </li>
                 <li>
-                    <Link to="/About" className="py-7 px-3 inline-block border-b">About</Link>
+                    <Link to="/about" className="py-7 px-3 inline-block border-b">About</Link>
                 </li>
             </ul>
         </div>
